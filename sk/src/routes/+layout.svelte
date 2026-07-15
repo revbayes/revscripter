3952ceebda8455b871e0ad8b9e799423c83f3975
brpetrucci <petrucci@iastate.edu>
2026-07-15 12:20:37 -0500

    Started on the layout

--- sk/src/routes/+layout.svelte
+++ sk/src/routes/+layout.svelte
@@ -8,4 +8,14 @@
 	<link rel="icon" href={revbayessymbol} />
 </svelte:head>
 
+<nav>
+	<button><a href="/">Home</a></button>
+	<a href="/data">Data</a>
+	<a href="/taxa">Taxa</a>
+	<a href="/substitution_model">Substitution model</a>
+	<a href="/clock_model">Clock model</a>
+	<a href="/priors">Priors</a>
+	<a href="/mcmc">MCMC</a>
+</nav>
+
 {@render children()}
